8c5f704f2dff8620c3a8c854fa2277b6df85e0f0
iS-AJ <alfredo@isolpa.com>
2026-02-16 15:09:50 -0500

    pricelist alias for compact pricing display and display UI fixes.

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'iS Pricelist Visibility',
-    'version': '17.0.1.0.4',
+    'version': '17.0.1.0.5',
     'category': 'Sales',
     'summary': 'Show configurable pricelist prices in product views',
     'description': """
